a691d323eb5512c9d5e7006249460ed2d5c591e9
whoisamyy <egegegg002@gmail.com>
2024-04-14 23:12:00 +0300

    tf

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation(project(":lib"))
     implementation("org.jetbrains:annotations:24.0.0")
     antlr("org.antlr:antlr4:4.13.1")
-
+    
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
