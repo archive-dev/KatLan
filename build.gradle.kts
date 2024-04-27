802dbad02821df2a369e1a2a6d64430d5454bcf3
whoisamyy <egegegg002@gmail.com>
2024-04-27 21:30:56 +0300

    ~ moved everything in one module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    antlr
 }
 
 group = "org.katarine"
@@ -10,8 +11,20 @@ repositories {
 }
 
 dependencies {
+    implementation("org.cojen:cojen-maker:2.5.6")
+    // https://mvnrepository.com/artifact/com.google.guava/guava
+    implementation("com.google.guava:guava:33.1.0-jre")
+
+//    implementation("org.jetbrains:annotations:24.0.0")
+    antlr("org.antlr:antlr4:4.13.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
 rootProject.name = "KatLan"
-include("lib")
-include("compiler")
