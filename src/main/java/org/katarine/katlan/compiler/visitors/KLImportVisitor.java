ffd015fb6ad444c4280015e961a4d57486a85f43
whoisamyy <egegegg002@gmail.com>
2024-05-05 00:24:19 +0300

    + added ClassMaker field

--- src/main/java/org/katarine/katlan/compiler/visitors/KLImportVisitor.java
+++ src/main/java/org/katarine/katlan/compiler/visitors/KLImportVisitor.java
@@ -1,11 +1,18 @@
 package org.katarine.katlan.compiler.visitors;
 
+import org.cojen.maker.ClassMaker;
 import org.katarine.katlan.compiler.antlr4.KatLanBaseVisitor;
 import org.katarine.katlan.compiler.antlr4.KatLanParser;
 
 import java.util.HashMap;
 
 public class KLImportVisitor extends KatLanBaseVisitor<HashMap<String, Object>> {
+    private ClassMaker cm;
+
+    public KLImportVisitor(ClassMaker cm) {
+        this.cm = cm;
+    }
+
     @Override
     public HashMap<String, Object> visitImportBlock(KatLanParser.ImportBlockContext ctx) {
         HashMap<String, Object> map = new HashMap<>();
