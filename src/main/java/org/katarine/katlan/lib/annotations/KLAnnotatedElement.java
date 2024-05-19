73f1203774dc02c59baf745186b678c9c9a94c34
whoisamyy <egegegg002@gmail.com>
2024-05-19 21:04:40 +0300

    ~ made extend java's AnnotatedElement

--- src/main/java/org/katarine/katlan/lib/annotations/KLAnnotatedElement.java
+++ src/main/java/org/katarine/katlan/lib/annotations/KLAnnotatedElement.java
@@ -1,8 +1,27 @@
 package org.katarine.katlan.lib.annotations;
 
 import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.Arrays;
 
-public interface KLAnnotatedElement extends Serializable {
+public interface KLAnnotatedElement extends Serializable, AnnotatedElement {
     Target.KLTarget getThisTargetType();
     KLAnnotation[] getKlAnnotations();
+
+    @SuppressWarnings("unchecked")
+    @Override
+    default <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+        return (T) Arrays.stream(getAnnotations()).filter(a -> a.getClass().equals(annotationClass)).findFirst().orElse(null);
+    }
+
+    @Override
+    default Annotation[] getDeclaredAnnotations() {
+        return getAnnotations();
+    }
+
+    @Override
+    default Annotation[] getAnnotations() {
+        return getKlAnnotations();
+    }
 }
