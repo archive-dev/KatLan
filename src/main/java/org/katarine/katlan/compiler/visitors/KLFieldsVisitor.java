362a375e0e52590d58dc7f3153d814ff20476aef
whoisamyy <egegegg002@gmail.com>
2024-05-19 21:15:22 +0300

    + added visitNamespaceBlock method ~ "fixed" visitUnnamedClassDef method + added visitAnnotationDef method and addFields for annotationClassBlockContext method

--- src/main/java/org/katarine/katlan/compiler/visitors/KLFieldsVisitor.java
+++ src/main/java/org/katarine/katlan/compiler/visitors/KLFieldsVisitor.java
@@ -6,6 +6,7 @@ import org.katarine.katlan.compiler.antlr4.KatLanParser;
 import org.antlr.v4.runtime.misc.Pair;
 import org.cojen.maker.ClassMaker;
 import org.cojen.maker.FieldMaker;
+import org.katarine.katlan.lib.FieldLink;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,7 +26,13 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
     }
 
     @Override
+    public HashMap<String, FieldMaker> visitNamespaceBlock(KatLanParser.NamespaceBlockContext ctx) {
+        return addFields(ctx);
+    }
+
+    @Override
     public HashMap<String, FieldMaker> visitUnnamedClassDef(KatLanParser.UnnamedClassDefContext ctx) {
+        if (ctx.classBlock()==null) return new HashMap<>();
         return addFields(ctx.classBlock());
     }
 
@@ -56,7 +63,37 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
         return map;
     }
 
-    private HashMap<String, FieldMaker> addFields(KatLanParser.ClassBlockContext cb) {
+    @Override
+    public HashMap<String, FieldMaker> visitAnnotationDef(KatLanParser.AnnotationDefContext ctx) {
+        return addFields(ctx.annotationClassBlock());
+    }
+
+    private HashMap<String, FieldMaker> addFields(KatLanParser.AnnotationClassBlockContext cb) {
+        final HashMap<String, FieldMaker> map = new HashMap<>();
+
+        if (cb==null) return map;
+        var vars = cb.var();
+        final HashMap<String, Object> fields = new HashMap<>();
+        var mm = cm.addMethod(null, "_").private_();
+        for (var v : vars) {
+            var field = addField(v);
+            field.forEach(p -> {
+                map.put(p.a, p.b);
+                fields.put(p.a, mm.field(p.a).classType());
+            });
+        }
+
+        int i = 0;
+        var constr = cm.addConstructor(fields.values().toArray(Object[]::new));
+        constr.invokeSuperConstructor();
+        for (var f : fields.entrySet()) {
+            constr.field(f.getKey()).set(constr.param(i++));
+        }
+
+        return map;
+    }
+
+    private HashMap<String, FieldMaker> addFields(KatLanParser.NamespaceBlockContext cb) {
         final HashMap<String, FieldMaker> map = new HashMap<>();
 
         if (cb==null) return map;
@@ -68,6 +105,17 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
         return map;
     }
 
+    private HashMap<String, FieldMaker> addFields(KatLanParser.ClassBlockContext cb) {
+        final HashMap<String, FieldMaker> map = new HashMap<>();
+
+        if (cb==null) return map;
+        var vars = cb.var();
+        for (var v : vars) {
+            var field = addField(v);
+            field.forEach(p -> map.put(p.a, p.b));
+        }
+        return map;
+    }
 
     private HashSet<Pair<String, FieldMaker>> addConst(KatLanParser.ConstDefContext cc) {
         HashSet<Pair<String, FieldMaker>> set = new HashSet<>();
@@ -94,6 +142,8 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
         return set;
     }
 
+    private int counter = 0;
+
     private HashSet<Pair<String, FieldMaker>> addField(KatLanParser.VarContext vc) {
         HashSet<Pair<String, FieldMaker>> set = new HashSet<>();
         var vd = vc.varDef();
@@ -101,6 +151,8 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
 
         var vd0 = vd.varDef0();
         var vd1 = vd.varDef1();
+
+        var clinit = compiler.getClinitVars().get("fieldLinks").methodMaker();
 
         if (vd0 != null) {
             var vName = vd0.name().getText();
@@ -114,8 +166,13 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
             if (vd0.value()!=null) {
                 System.out.println("sorry! no init");
             }
+            var fieldLink = clinit.var(FieldLink.class);
+            fieldLink.set(clinit.new_(FieldLink.class, clinit.class_(), fm.name()));
+
+            compiler.getClinitVars().get("fieldLinks").aset(counter, fieldLink);
 
             set.add(new Pair<>(vName, fm));
+            counter++;
         } else {
             var vType = compiler.imports.get(vd1.type().getText());
             for (var n : vd1.subVD1()) {
@@ -126,9 +183,16 @@ public class KLFieldsVisitor extends KatLanBaseVisitor<HashMap<String, FieldMake
                 if (vd.access().STATIC() != null)    fm.static_();
                 if (vd.access().FINAL() != null)     fm.final_();
 
+                var fieldLink = clinit.var(FieldLink.class);
+                fieldLink.set(clinit.new_(FieldLink.class, clinit.class_(), fm.name()));
+
+                compiler.getClinitVars().get("fieldLinks").aset(counter, fieldLink);
+
                 set.add(new Pair<>(n.getText(), fm));
+                counter++;
             }
         }
+
         return set;
     }
 }
