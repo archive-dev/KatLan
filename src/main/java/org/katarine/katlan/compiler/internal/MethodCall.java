70acd672c3fd7b9f440dd02f76f16fd0b50f15e2
whoisamyy <egegegg002@gmail.com>
2024-05-19 21:17:56 +0300

    - removed ImmutableArrayList extension and all related things ~ changed ImmutableArrayList to a plain array ~ refactor

--- src/main/java/org/katarine/katlan/compiler/internal/MethodCall.java
+++ src/main/java/org/katarine/katlan/compiler/internal/MethodCall.java
@@ -2,61 +2,26 @@ package org.katarine.katlan.compiler.internal;
 
 import org.katarine.katlan.compiler.annotations.KLAnnotationWrapper;
 import org.katarine.katlan.lib.MethodLink;
-import org.katarine.katlan.lib.annotations.Annotations;
-import org.katarine.katlan.lib.annotations.KLAnnotatedElement;
-import org.katarine.katlan.lib.annotations.KLAnnotation;
-import org.katarine.katlan.lib.annotations.Target;
-import org.katarine.katlan.lib.structs.ImmutableArrayList;
+import org.katarine.katlan.lib.annotations.*;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.Hashtable;
-import java.util.List;
 
 public class MethodCall implements KLAnnotatedElement {
     public final Object caller;
     public final Object[] args;
 
-    public final ImmutableList<KLAnnotation> annotations = new ImmutableList<>();
     public final MethodLink ml;
+    private KLAnnotation[] annotations;
 
     private final Hashtable<KLAnnotationWrapper<?>, KLAnnotation> wrappers;
-
-    public class ImmutableList<E extends KLAnnotation> extends ImmutableArrayList<E> {
-        @Override
-        public boolean add(E t) {
-            wrappers.put(new KLAnnotationWrapper<>(t), t);
-            return super.add(t);
-        }
-
-        @Override
-        public void add(int index, E element) {
-            wrappers.put(new KLAnnotationWrapper<>(element), element);
-            super.add(index, element);
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends E> c) {
-            for (var annotation : c) {
-                wrappers.put(new KLAnnotationWrapper<>(annotation), annotation);
-            }
-            return super.addAll(c);
-        }
-
-        @Override
-        public boolean addAll(int index, Collection<? extends E> c) {
-            for (var annotation : c) {
-                wrappers.put(new KLAnnotationWrapper<>(annotation), annotation);
-            }
-            return super.addAll(index, c);
-        }
-    }
 
     public MethodCall(MethodLink ml, Object caller, Object[] args, KLAnnotation... annotations) {
         this.ml = ml;
         this.caller = caller;
         this.args = args;
-        this.annotations.addAll(List.of(annotations));
+        this.annotations = annotations;
         this.wrappers = new Hashtable<>();
         for (var annotation : annotations) {
             wrappers.put(new KLAnnotationWrapper<>(annotation), annotation);
@@ -66,7 +31,7 @@ public class MethodCall implements KLAnnotatedElement {
     @SuppressWarnings("unchecked")
     public <T> T call() {
         for (var wrapper : wrappers.entrySet()) {
-            wrapper.getKey().handle(wrapper.getValue(), Annotations.CallType.PRE_METHOD_CALL);
+            wrapper.getKey().handle(wrapper.getValue(), CallType.PRE_METHOD_CALL);
         }
         try {
             return (T) ml.invoke(caller, args);
@@ -74,7 +39,7 @@ public class MethodCall implements KLAnnotatedElement {
             throw new RuntimeException(e);
         } finally {
             for (var wrapper : wrappers.entrySet()) {
-                wrapper.getKey().handle(wrapper.getValue(), Annotations.CallType.POST_METHOD_CALL);
+                wrapper.getKey().handle(wrapper.getValue(), CallType.POST_METHOD_CALL);
             }
         }
     }
@@ -86,6 +51,6 @@ public class MethodCall implements KLAnnotatedElement {
 
     @Override
     public KLAnnotation[] getKlAnnotations() {
-        return this.annotations.toArray(KLAnnotation[]::new);
+        return Arrays.copyOf(annotations, annotations.length);
     }
 }
