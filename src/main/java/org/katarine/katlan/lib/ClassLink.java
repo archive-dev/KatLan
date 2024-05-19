54db9aeacbcc5d2f95a54fa15b9619b134ee2609
whoisamyy <egegegg002@gmail.com>
2024-05-19 21:08:04 +0300

    ~ made all fields private ~ changed all `ImmutableArrayList`s to plain arrays + added factory method (`of(Class<?>)`) + added getters

--- src/main/java/org/katarine/katlan/lib/ClassLink.java
+++ src/main/java/org/katarine/katlan/lib/ClassLink.java
@@ -1,53 +1,69 @@
 package org.katarine.katlan.lib;
 
-import org.katarine.katlan.compiler.internal.Packages;
 import org.katarine.katlan.lib.annotations.KLAnnotatedElement;
 import org.katarine.katlan.lib.annotations.KLAnnotation;
 import org.katarine.katlan.lib.annotations.Target;
-import org.katarine.katlan.lib.structs.ImmutableArrayList;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 public class ClassLink implements Serializable, KLAnnotatedElement { // @ClassName
-    private final Class<?> type;
-    public final ClassType classType;
-    public final boolean isFinal;
-    public final String name;
+    private final Class<?> jClass;
+    private final ClassType classType;
+    private final boolean isFinal;
+    private final boolean isAnnotation;
+    private final String name;
     private final Method[] methods;
-    public final ImmutableArrayList<MethodLink> methodLinks = new ImmutableArrayList<>();
-    public final Field[] fields;
-    public final Class<?> superClass;
-    public final ImmutableArrayList<KLAnnotation> klAnnotations = new ImmutableArrayList<>();
-    public final KLPackage pkg;
+    private final Field[] fields;
+    private MethodLink[] methodLinks;
+    private FieldLink[] fieldLinks;
+    private OperatorOverrideMethodLink[] operatorOverrides;
+    private KLAnnotation[] klAnnotations;
+    private final ClassLink superClass;
+    private final KLPackage pkg;
 
-    public ClassLink(Class<?> type, KLAnnotation[] klAnnotations) {
-        this.type = type;
-        this.name = this.type.getSimpleName();
-        this.methods = this.type.getDeclaredMethods();
-        this.fields = this.type.getDeclaredFields();
-        this.superClass = this.type.getSuperclass();
-
-        List<MethodLink> mls = new ArrayList<>(Arrays.stream(methods).map(MethodLink::new).toList());
-        this.methodLinks.addAll(mls);
-
-        this.klAnnotations.addAll(List.of(klAnnotations));
-        if (Modifier.isAbstract(this.type.getModifiers())) this.classType = ClassType.ABSTRACT;
-        else if (Modifier.isInterface(this.type.getModifiers())) this.classType = ClassType.INTERFACE;
-        else this.classType = ClassType.CLASS;
-
-        this.isFinal = Modifier.isFinal(this.type.getModifiers());
-        this.pkg = Packages.getPackage(this.type.getPackageName());
+    public static ClassLink of(Class<?> jClass) {
+        ClassLink cl = Classes.loadClassLink(jClass);
+        if (cl == null) cl = new ClassLink(jClass);
+        return cl;
     }
 
-    public ClassLink(Class<?> type) {
-        this(type, new KLAnnotation[0]);
+    public ClassLink(Class<?> jClass, MethodLink[] methodLinks, FieldLink[] fieldLinks, KLAnnotation[] klAnnotations) {
+        Classes.registerClass(this);
+        this.jClass = jClass;
+        this.isAnnotation = KLAnnotation.class.isAssignableFrom(this.jClass);
+        this.name = this.jClass.getSimpleName();
+        this.methods = this.jClass.getDeclaredMethods();
+        this.fields = this.jClass.getDeclaredFields();
+        this.superClass = of(this.jClass.getSuperclass());
+
+        this.methodLinks = methodLinks;
+        this.fieldLinks = fieldLinks;
+
+        this.klAnnotations = klAnnotations;
+        if (Modifier.isAbstract(this.jClass.getModifiers())) this.classType = ClassType.ABSTRACT;
+        else if (Modifier.isInterface(this.jClass.getModifiers())) this.classType = ClassType.INTERFACE;
+        else this.classType = ClassType.CLASS;
+
+        this.isFinal = Modifier.isFinal(this.jClass.getModifiers());
+        this.pkg = Packages.getPackage(this.jClass.getPackageName());
+    }
+
+    public ClassLink(Class<?> jClass, KLAnnotation[] klAnnotations) {
+        this(
+                jClass,
+                Arrays.stream(jClass.getDeclaredMethods()).map(MethodLink::new).toArray(MethodLink[]::new),
+                Arrays.stream(jClass.getDeclaredFields()).map(FieldLink::new).toArray(FieldLink[]::new),
+                klAnnotations
+        );
+    }
+
+    public ClassLink(Class<?> jClass) {
+        this(jClass, new KLAnnotation[0]);
     }
 
     public final Method getMethod(String name, Class<?> returnType, Class<?>... argTypes) throws NoSuchMethodException {
@@ -59,26 +75,20 @@ public class ClassLink implements Serializable, KLAnnotatedElement { // @ClassNa
 
     public final MethodLink getMethodLink(String name, Class<?> returnType, Class<?>... argTypes) throws NoSuchMethodException {
         MethodLink ml = getMethodLink(name, argTypes);
-        if (!ml.returnType.equals(returnType))
+        if (!ml.getReturnType().equals(returnType))
             throw new NoSuchMethodException("No such method: " + name + " " + Arrays.toString(argTypes));
         return ml;
     }
 
     public final MethodLink getMethodLink(String name, Class<?>... argTypes) {
-        this.methodLinks.makeImmutable();
-        this.klAnnotations.makeImmutable();
-
         for (var m : methodLinks) {
-            if (Objects.equals(name, m.methodName) && Arrays.equals(m.parameterTypes, argTypes))
+            if (Objects.equals(name, m.getName()) && Arrays.equals(m.getParameterTypes(), argTypes))
                 return m;
         }
         throw new RuntimeException(new NoSuchMethodException("No such method: " + name + " " + Arrays.toString(argTypes)));
     }
 
     public final Method getMethod(String name, Class<?>... argTypes) throws NoSuchMethodException {
-        this.methodLinks.makeImmutable();
-        this.klAnnotations.makeImmutable();
-
         for (var m : methods) {
             if (Objects.equals(name, m.getName()) && Arrays.equals(m.getParameterTypes(), argTypes))
                 return m;
@@ -87,9 +97,6 @@ public class ClassLink implements Serializable, KLAnnotatedElement { // @ClassNa
     }
 
     public final Field getField(String name) throws NoSuchFieldException {
-        this.methodLinks.makeImmutable();
-        this.klAnnotations.makeImmutable();
-
         for (var f : fields) {
             if (Objects.equals(name, f.getName()))
                 return f;
@@ -104,6 +111,50 @@ public class ClassLink implements Serializable, KLAnnotatedElement { // @ClassNa
 
     @Override
     public KLAnnotation[] getKlAnnotations() {
-        return this.klAnnotations.toArray(KLAnnotation[]::new);
+        return Arrays.copyOf(klAnnotations, klAnnotations.length);
+    }
+
+    public final Class<?> jClass() {
+        return jClass;
+    }
+
+    public final ClassType getClassType() {
+        return classType;
+    }
+
+    public final boolean isFinal() {
+        return isFinal;
+    }
+
+    public final boolean isAnnotation() {
+        return isAnnotation;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public final Method[] getMethods() {
+        return methods;
+    }
+
+    public final Field[] getFields() {
+        return fields;
+    }
+
+    public final MethodLink[] getMethodLinks() {
+        return methodLinks;
+    }
+
+    public final OperatorOverrideMethodLink[] getOperatorOverrides() {
+        return operatorOverrides;
+    }
+
+    public final ClassLink getSuperClass() {
+        return superClass;
+    }
+
+    public final KLPackage getPkg() {
+        return pkg;
     }
 }
