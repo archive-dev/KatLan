14cb5f56e2f5dab1761a3882147cce0875e1b99c
whoisamyy <egegegg002@gmail.com>
2024-05-29 00:16:09 +0300

    ~ no longer implements java.lang.reflect.AnnotatedElement

--- src/main/java/org/katarine/katlan/lib/annotations/KLAnnotatedElement.java
+++ src/main/java/org/katarine/katlan/lib/annotations/KLAnnotatedElement.java
@@ -5,23 +5,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.util.Arrays;
 
-public interface KLAnnotatedElement extends Serializable, AnnotatedElement {
+public interface KLAnnotatedElement extends Serializable {
     Target.KLTarget getThisTargetType();
     KLAnnotation[] getKlAnnotations();
-
-    @SuppressWarnings("unchecked")
-    @Override
-    default <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
-        return (T) Arrays.stream(getAnnotations()).filter(a -> a.getClass().equals(annotationClass)).findFirst().orElse(null);
-    }
-
-    @Override
-    default Annotation[] getDeclaredAnnotations() {
-        return getAnnotations();
-    }
-
-    @Override
-    default Annotation[] getAnnotations() {
-        return getKlAnnotations();
-    }
 }
