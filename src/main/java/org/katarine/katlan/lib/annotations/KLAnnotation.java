93fa7524fa2263714cb594311ca1e376d0a88e61
whoisamyy <egegegg002@gmail.com>
2024-05-29 00:13:05 +0300

    ~ no longer implements java.lang.annotation.Annotation

--- src/main/java/org/katarine/katlan/lib/annotations/KLAnnotation.java
+++ src/main/java/org/katarine/katlan/lib/annotations/KLAnnotation.java
@@ -5,14 +5,40 @@ import org.cojen.maker.MethodMaker;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 import java.util.ArrayList;
 
-public abstract class KLAnnotation implements Serializable, Annotation {
+public abstract class KLAnnotation implements Serializable {
     public final KLAnnotatedElement annotatedObject;
 
     public KLAnnotation(KLAnnotatedElement annotatedObject) {
         this.annotatedObject = annotatedObject;
         Annotations.registerAnnotation(this);
+    }
+
+    /**
+     * Creates a java annotation class from klAnnotation class
+     * @param klAnnotation KLAnnotation class to generate from
+     * @return Java annotation Class.
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends Annotation> getJavaAnnotationFromKLAnnotation(Class<? extends KLAnnotation> klAnnotation) {
+        ClassMaker cm = ClassMaker.beginExplicit(klAnnotation.getSimpleName()+"J", klAnnotation.getClassLoader(), null);
+        cm.annotation();
+//        ClassLink klClass = klAnnotation.getDeclaredField("klclass").get(null);
+//        klClass.get
+
+        for (var f : klAnnotation.getDeclaredFields()) {
+            if (!f.getType().isPrimitive() || !f.getType().equals(String.class)) continue;
+
+            cm.addMethod(f.getType(), f.getName());
+        }
+
+        cm.addAnnotation(Target.class, true).put("value", ElementType.TYPE);
+
+
+        return (Class<? extends Annotation>) cm.finish();
     }
 
     public static Class<? extends KLAnnotation> from(Class<? extends Annotation> annotation) {
@@ -38,10 +64,5 @@ public abstract class KLAnnotation implements Serializable, Annotation {
         @SuppressWarnings("unchecked")
         Class<? extends KLAnnotation> ret = (Class<? extends KLAnnotation>) cm.finish();
         return ret;
-    }
-
-    @Override
-    public final Class<? extends Annotation> annotationType() {
-        return this.getClass();
     }
 }
