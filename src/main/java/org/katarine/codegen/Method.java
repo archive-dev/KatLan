cca45ad02b48f9e41e64273433bfbc090a0e30f2
whoisamyy <egegegg002@gmail.com>
2024-07-18 21:32:15 +0300

    ~ made lazy initialization for method scope. there are reasons for it ~ little refactor + added return_() method

--- src/main/java/org/katarine/codegen/Method.java
+++ src/main/java/org/katarine/codegen/Method.java
@@ -7,35 +7,42 @@ import org.objectweb.asm.Opcodes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unchecked")
 public class Method implements Member, Accessible {
     private final ClassGenerator owner;
-
     private final String name;
-    private final ArrayList<Type> parameterTypes = new ArrayList<>();
-    private final ArrayList<String> parameterNames = new ArrayList<>();
+    private final List<Type> parameterTypes = new ArrayList<>();
+    private final List<String> parameterNames = new ArrayList<>();
     private final Type returnType;
     private final Signature signature;
 
     private int modifiers = Opcodes.ACC_PUBLIC;
-
     private final Supplier<MethodVisitor> mvProvider;
 
+    // lazy initialization because of methodscope constructor
+    private MethodScope ms;
+    private final Supplier<MethodScope> mainScope = new Supplier<>() {
+        @Override
+        public MethodScope get() {
+            if (ms!=null) return ms;
+            return ms = new MethodScope(Method.this);
+        }
+    };
     private final ArrayList<Consumer<MethodVisitor>> opQueue = new ArrayList<>();
 
-    private final MethodScope mainScope = new MethodScope(this);
+    private boolean isStatic = false;
+    private boolean isFinal = false;
+    private boolean isPublic = false;
+    private boolean isPrivate = false;
+    private boolean isProtected = false;
+    private boolean isAbstract = false;
 
-    public Method(ClassGenerator owner, String name, String signature, Type returnType, Type... parameterTypes) {
-        this(owner, name, new Signature(signature), returnType, parameterTypes);
-    }
-
-
-    public Method(ClassGenerator owner, String name, String signature, Type returnType, Pair<String, Type>[] parameters) {
-        this(owner, name, new Signature(signature), returnType, parameters);
-    }
+    private HashMap<String, Type> cachedParameters;
 
     public Method(ClassGenerator owner, String name, Signature signature, Type returnType, Pair<String, Type>[] parameters) {
         this.owner = owner;
@@ -44,34 +51,48 @@ public class Method implements Member, Accessible {
         this.parameterTypes.addAll(Arrays.stream(parameters).map(Pair::getB).toList());
         this.returnType = returnType;
         this.signature = signature;
-
-        mvProvider = () -> owner.getClassWriter().visitMethod(modifiers, name, getDescriptor(), signature.getSignature(), null);
+        this.mvProvider = () -> owner.getClassWriter().visitMethod(modifiers, name, getDescriptor(), signature.getSignature(), null);
     }
 
-    @SuppressWarnings("unchecked")
     public Method(ClassGenerator owner, String name, Signature signature, Type returnType, Type... parameterTypes) {
-        final ArrayList<Pair<String, Type>> parameters = new ArrayList<>();
-        for (int i = 0; i < parameterTypes.length; i++) {
-            parameters.add(new Pair<>("var%d".formatted(i), parameterTypes[i]));
-        }
-
-        this(owner, name, signature, returnType, parameters.toArray(Pair[]::new));
-        // this is why I updated to java 22 :D
+        this(owner, name, signature, returnType, createParameterPairs(parameterTypes));
     }
 
-    public void addInsn(Consumer<MethodVisitor> insnCode) {
-        this.opQueue.add(insnCode);
+    public Method(ClassGenerator owner, String name, String signature, Type returnType, Pair<String, Type>[] parameters) {
+        this(owner, name, new Signature(signature), returnType, parameters);
+    }
+
+    public Method(ClassGenerator owner, String name, String signature, Type returnType, Type... parameterTypes) {
+        this(owner, name, new Signature(signature), returnType, createParameterPairs(parameterTypes));
+    }
+
+    private static Pair<String, Type>[] createParameterPairs(Type... parameterTypes) {
+        Pair<String, Type>[] parameters = new Pair[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++) {
+            parameters[i] = new Pair<>("var" + i, parameterTypes[i]);
+        }
+        return parameters;
+    }
+
+    void addInsn(Consumer<MethodVisitor> insn) {
+        this.opQueue.add(insn);
+    }
+
+    void addInsns(List<Consumer<MethodVisitor>> insns) {
+        this.opQueue.addAll(insns);
     }
 
     public Variable var(Type type, String name) {
-        return mainScope.createVariable(type, name);
+        return mainScope.get().createVariable(type, name);
+    }
+
+    public void return_() {
+        opQueue.add(mv -> mv.visitInsn(Opcodes.RETURN));
     }
 
     public String getDescriptor() {
-        String ret = "(";
-        ret += String.join("", parameterTypes.stream().map(Type::getDescriptor).toList()) + ")";
-        ret += returnType.getDescriptor();
-        return ret;
+        String paramDescriptors = parameterTypes.stream().map(Type::getDescriptor).collect(Collectors.joining());
+        return "(" + paramDescriptors + ")" + returnType.getDescriptor();
     }
 
     public ClassGenerator getOwner() {
@@ -86,109 +107,79 @@ public class Method implements Member, Accessible {
         return signature;
     }
 
-    @SuppressWarnings("unchecked")
-    public ArrayList<Type> getParameterTypes() {
-        return (ArrayList<Type>) parameterTypes.clone();
+    public List<Type> getParameterTypes() {
+        return new ArrayList<>(parameterTypes);
     }
 
-
-    private HashMap<String, Type> cachedParameters;
     public HashMap<String, Type> getParameters() {
-        if (cachedParameters==null)
+        if (cachedParameters == null || cachedParameters.size() != parameterTypes.size()) {
             cachedParameters = new HashMap<>();
-
-        if (cachedParameters.size()!=parameterTypes.size()) {
             for (int i = 0; i < parameterTypes.size(); i++) {
                 cachedParameters.put(parameterNames.get(i), parameterTypes.get(i));
             }
         }
-
         return cachedParameters;
     }
 
     @Override
-    public final void end() {
+    public void end() {
         MethodVisitor mv = mvProvider.get();
-
-        opQueue.addLast((visitor) -> visitor.visitMaxs(0, 0));
-        opQueue.forEach((op) -> op.accept(mv));
+        opQueue.addLast(visitor -> visitor.visitMaxs(0, 0));
+        opQueue.forEach(op -> op.accept(mv));
     }
 
     @Override
-    public boolean isStatic() {
-        return isStatic;
-    }
-
+    public boolean isStatic() { return isStatic; }
     @Override
-    public boolean isFinal() {
-        return isFinal;
-    }
-
+    public boolean isFinal() { return isFinal; }
     @Override
-    public boolean isPublic() {
-        return isPublic;
-    }
-
+    public boolean isPublic() { return isPublic; }
     @Override
-    public boolean isPrivate() {
-        return isPrivate;
-    }
-
+    public boolean isPrivate() { return isPrivate; }
     @Override
-    public boolean isProtected() {
-        return isProtected;
-    }
-
+    public boolean isProtected() { return isProtected; }
     @Override
-    public boolean isAbstract() {
-        return isAbstract;
-    }
+    public boolean isAbstract() { return isAbstract; }
 
-    private boolean isStatic = false;
     @Override
     public <T extends Accessible> T static_() {
         this.modifiers |= Opcodes.ACC_STATIC;
-        isStatic = true;
+        this.isStatic = true;
         return (T) this;
     }
 
-    private boolean isFinal = false;
     @Override
     public <T extends Accessible> T final_() {
         this.modifiers |= Opcodes.ACC_FINAL;
-        isFinal = true;
+        this.isFinal = true;
         return (T) this;
     }
 
-    private boolean isPublic = false;
     @Override
     public <T extends Accessible> T public_() {
         this.modifiers |= Opcodes.ACC_PUBLIC;
-        isPublic = true;
+        this.isPublic = true;
         return (T) this;
     }
 
-    private boolean isPrivate = false;
     @Override
     public <T extends Accessible> T private_() {
         this.modifiers |= Opcodes.ACC_PRIVATE;
-        isPrivate = true;
+        this.isPrivate = true;
         return (T) this;
     }
 
-    private boolean isProtected = false;
     @Override
     public <T extends Accessible> T protected_() {
         this.modifiers |= Opcodes.ACC_PROTECTED;
-        isProtected = true;
+        this.isProtected = true;
         return (T) this;
     }
 
-    private boolean isAbstract = false;
     @Override
     public <T extends Accessible> T abstract_() {
         this.modifiers |= Opcodes.ACC_ABSTRACT;
-        isAbstract = true;
+        this.isAbstract = true;
         return (T) this;
     }
 }
