888bb678912237ea3e4af763c70cdc28f280a5ec
whoisamyy <egegegg002@gmail.com>
2024-08-09 23:08:05 +0300

    ~ fixed method descriptors

--- src/main/java/org/katarine/codegen/Caller.java
+++ src/main/java/org/katarine/codegen/Caller.java
@@ -1,9 +1,7 @@
 package org.katarine.codegen;
 
 public interface Caller {
-    void invokeVoid(MethodType type, String name, Object[] arguments);
-    Variable invoke(Type returnType, MethodType type, String name, Object[] arguments);
-    default Variable invoke(Method method, Object[] arguments) {
-        return invoke(method.getReturnType(), method.getMethodType(), method.getName(), arguments);
-    }
+    void invokeVoid(Variable owner, MethodType type, String name, Object[] arguments);
+    Variable invoke(Variable owner, Type returnType, MethodType type, String name, Object[] arguments);
+    Variable invoke(Variable owner, Method method, Object[] arguments);
 }
