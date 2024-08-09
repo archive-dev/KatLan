3b67df8fa342d0edca7b269bddf14a47cd9da10d
whoisamyy <egegegg002@gmail.com>
2024-08-09 23:02:16 +0300

    + added more utility methods ~ changed consts types

--- src/main/java/org/katarine/codegen/Type.java
+++ src/main/java/org/katarine/codegen/Type.java
@@ -9,15 +9,15 @@ import org.objectweb.asm.Opcodes;
  */
 @SuppressWarnings("StaticInitializerReferencesSubClass")
 public sealed class Type permits Type.PrimitiveType, Type.SimpleType {
-    public static final Type INT     = new PrimitiveType(int.class);
-    public static final Type DOUBLE  = new PrimitiveType(double.class);
-    public static final Type FLOAT   = new PrimitiveType(float.class);
-    public static final Type LONG    = new PrimitiveType(long.class);
-    public static final Type SHORT   = new PrimitiveType(short.class);
-    public static final Type BYTE    = new PrimitiveType(byte.class);
-    public static final Type BOOLEAN = new PrimitiveType(boolean.class);
-    public static final Type CHAR    = new PrimitiveType(char.class);
-    public static final Type VOID    = new PrimitiveType(void.class);
+    public static final PrimitiveType INT     = new PrimitiveType(int.class);
+    public static final PrimitiveType DOUBLE  = new PrimitiveType(double.class);
+    public static final PrimitiveType FLOAT   = new PrimitiveType(float.class);
+    public static final PrimitiveType LONG    = new PrimitiveType(long.class);
+    public static final PrimitiveType SHORT   = new PrimitiveType(short.class);
+    public static final PrimitiveType BYTE    = new PrimitiveType(byte.class);
+    public static final PrimitiveType BOOLEAN = new PrimitiveType(boolean.class);
+    public static final PrimitiveType CHAR    = new PrimitiveType(char.class);
+    public static final PrimitiveType VOID    = new PrimitiveType(void.class);
 
     public static final Type OBJECT  = new Type(Object.class);
 
@@ -49,12 +49,75 @@ public sealed class Type permits Type.PrimitiveType, Type.SimpleType {
 
     public static final class PrimitiveType extends Type {
         PrimitiveType(Class<?> clazz) {
+            if (!clazz.isPrimitive()) throw new IllegalArgumentException();
             super(clazz);
         }
 
         @Override
         public boolean isPrimitive() {
             return true;
+        }
+
+        public static int getCastCode(PrimitiveType a, PrimitiveType b) {
+            if (a.equals(INT) || a.equals(BYTE) || a.equals(BOOLEAN) || a.equals(CHAR) || a.equals(SHORT)) {
+                return getIntCastCode(b);
+            } else if (a.equals(LONG)) {
+                return getLongCastCode(b);
+            } else if (a.equals(FLOAT)) {
+                return getFloatCastCode(b);
+            } else if (a.equals(DOUBLE)) {
+                return getDoubleCastCode(b);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        private static int getDoubleCastCode(PrimitiveType other) {
+            if (other.equals(INT)) {
+                return Opcodes.D2I;
+            } else if (other.equals(LONG)) {
+                return Opcodes.D2L;
+            } else if (other.equals(FLOAT)) {
+                return Opcodes.D2F;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        private static int getFloatCastCode(PrimitiveType other) {
+            if (other.equals(INT)) {
+                return Opcodes.F2I;
+            } else if (other.equals(LONG)) {
+                return Opcodes.F2L;
+            } else if (other.equals(DOUBLE)) {
+                return Opcodes.F2D;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        private static int getLongCastCode(PrimitiveType other) {
+            if (other.equals(INT)) {
+                return Opcodes.L2I;
+            } else if (other.equals(FLOAT)) {
+                return Opcodes.L2F;
+            } else if (other.equals(DOUBLE)) {
+                return Opcodes.L2D;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        private static int getIntCastCode(PrimitiveType other) {
+            if (other.equals(BYTE)) {
+                return Opcodes.I2B;
+            } else if (other.equals(SHORT)) {
+                return Opcodes.I2S;
+            } else if (other.equals(LONG)) {
+                return Opcodes.I2L;
+            } else if (other.equals(FLOAT)) {
+                return Opcodes.I2F;
+            } else if (other.equals(DOUBLE)) {
+                return Opcodes.I2D;
+            }
+            throw new IllegalArgumentException();
         }
     }
 
@@ -79,6 +142,8 @@ public sealed class Type permits Type.PrimitiveType, Type.SimpleType {
     }
 
     public Type(String descriptor, Signature signature) {
+        if (TypeResolver.isPrimitive(descriptor))
+            throw new IllegalArgumentException("For primitive types use defined constants or use PrimitiveType class");
         this.descriptor = descriptor;
         this.signature = signature;
     }
@@ -118,7 +183,7 @@ public sealed class Type permits Type.PrimitiveType, Type.SimpleType {
             default -> throw new IllegalStateException("Unexpected value: " + this.getDescriptor());
         };
     }
-    
+
     public int getLoadCode() {
         if (!TypeResolver.isPrimitive(this.getDescriptor())) {
             if (!TypeResolver.isArray(this.getDescriptor()))
@@ -152,6 +217,30 @@ public sealed class Type permits Type.PrimitiveType, Type.SimpleType {
             case "F" -> Opcodes.FRETURN;
             case "D" -> Opcodes.DRETURN;
             case "J" -> Opcodes.LRETURN;
+            default -> throw new IllegalStateException("Unexpected value: " + this.getDescriptor());
+        };
+    }
+
+    public int[] getCmpEqCodes() {
+        if (!isPrimitive())
+            return new int[]{Opcodes.IF_ACMPEQ};
+        return switch (this.getDescriptor()) {
+            case "Z", "B", "C", "S", "I" -> new int[]{Opcodes.IF_ICMPEQ};
+            case "F" -> new int[]{Opcodes.FCMPL, Opcodes.IFEQ};
+            case "D" -> new int[]{Opcodes.DCMPL, Opcodes.IFEQ};
+            case "J" -> new int[]{Opcodes.LCMP, Opcodes.IFEQ};
+            default -> throw new IllegalStateException("Unexpected value: " + this.getDescriptor());
+        };
+    }
+
+    public int[] getCmpNeCodes() {
+        if (!isPrimitive())
+            return new int[]{Opcodes.IF_ACMPNE};
+        return switch (this.getDescriptor()) {
+            case "Z", "B", "C", "S", "I" -> new int[]{Opcodes.IF_ICMPNE};
+            case "F" -> new int[]{Opcodes.FCMPL, Opcodes.IFNE};
+            case "D" -> new int[]{Opcodes.DCMPL, Opcodes.IFNE};
+            case "J" -> new int[]{Opcodes.LCMP, Opcodes.IFNE};
             default -> throw new IllegalStateException("Unexpected value: " + this.getDescriptor());
         };
     }
