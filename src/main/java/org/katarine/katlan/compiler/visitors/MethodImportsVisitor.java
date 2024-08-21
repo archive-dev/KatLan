3db401c8afe660db8d3b436211f734db1c0a029a
whoisamyy <egegegg002@gmail.com>
2024-08-21 21:44:24 +0300

    ~ refactors and fixes

--- src/main/java/org/katarine/katlan/compiler/visitors/MethodImportsVisitor.java
+++ src/main/java/org/katarine/katlan/compiler/visitors/MethodImportsVisitor.java
@@ -1,6 +1,5 @@
 package org.katarine.katlan.compiler.visitors;
 
-import org.katarine.codegen.Type;
 import org.katarine.katlan.antlr4.KatLanBaseVisitor;
 import org.katarine.katlan.antlr4.KatLanParser;
 
@@ -16,20 +15,13 @@ public class MethodImportsVisitor extends KatLanBaseVisitor<HashMap<String, Stri
             return ret;
 
 
-        for (var statement : ctx.importMethod()) {
+        for (var statement : ctx.importStatic()) {
             ret.put(statement.AS_KEYWORD()==null ? Arrays.stream(statement.name(0).getText().split("\\."))
                             .toList()
                             .getLast() : statement.name(1).getText(),
-                    getImportedMethod(statement));
+                    statement.name(0).getText());
         }
 
         return ret;
-    }
-
-    private String getImportedMethod(KatLanParser.ImportMethodContext ctx) {
-        String methodName = Arrays.stream(ctx.name(0).getText().split("\\.")).toList().getLast();
-        String ownerClass = ctx.name(0).getText().split(methodName)[0];
-        ownerClass = ownerClass.substring(0, ownerClass.length()-1);
-        return new Type.SimpleType(ownerClass).getDescriptor() + "." + methodName;
     }
 }
